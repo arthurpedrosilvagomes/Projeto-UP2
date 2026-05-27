e8af8dee33b704d008f724b71dc1886d024d3b14
Arthur <arthurpedrosilvagomes1@gmail.com>
2026-05-27 20:35:19 -0300

    Aplicativo de lanche de cantina
    O aplicativo contém:
    interface do usuário em console;
    funcionalidades bem definidas;
    função cardápio desenvolvido (incompleto);
    
    Correção:
    Ao invés de varias Arryas para cada tipo de lanche, agora só tem um para todos;

--- main.cpp
+++ main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 
-int saldo = 0;
+int saldo = 50;
 
 void cardapio() {
-    std::string laches[] = {0};
-    int precos[] = {0};
+    std::string laches[9] = {"Pão de queijo", "Coxinha", "Pastel", "MiniPizza", "Brigadeido", "Surpresa de Uva", "Bolo", "Suco de laranja", "Suco de uva"};
+    float precos[9] = {1.20, 2.10, 2.50, 2.35, 1.50, 2, 3.50, 1.20, 1.20};
+
+    int escolher_lache;
+
+    while (escolher_lache != -1) {
+        for (int i = 0; i < 9; i++) {
+            std::cout << "[" << i + 1 << "] " << laches[i] << " - " << precos[i] << "\n";
+        }
+
+        std::cout << "\nSelecione um dos itens para comprar o lanche, ou digite '-1' para sair do cardápio\n"
+                  << ">>> ";
+        
+        std::cin >> escolher_lache;
+
+        if (escolher_lache == -1) {
+            return;
+        }
+
+        if (!(1 <= escolher_lache && escolher_lache <= 9)) {
+            std::cout << "O lanche que você busca não está aqui!\n";
+        }
+
+        else if (saldo < precos[escolher_lache - 1]) {
+            std::cout << "Você não tem saldo o suficiente!\n";
+        }
+
+        else {
+            std::cout << "Compra feita com sucesso!\n";
+            saldo = saldo - precos[escolher_lache - 1];
+        }
+    }
 }
 
 int main() {
@@ -29,6 +59,7 @@ int main() {
         switch (escolha_do_usuario) {
 
             case 1:
+                cardapio();
                 break;
             
             case 2:
